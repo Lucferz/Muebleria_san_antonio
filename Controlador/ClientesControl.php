be73e2ed6438b4c9ceff5fa15526a5afcbf78cf9
YisellRR <ravioloyisell@gmail.com>
2022-04-18 12:26:19 -0400

    ABM clientes creacion

---
+++ Controlador/ClientesControl.php
@@ -0,0 +1,29 @@
+<?php
+	
+	require_once("../DatosDAO/clientesDAO.php");
+	class ClientesControl
+	{
+		private $clientDAO;
+		public function __construct()
+		{
+			$this->clientDAO = new ClientesDAO();
+		}
+
+		public function __destruct(){
+			unset($this->clientDAO);
+		}
+
+		public function create($cliente = array()){
+			return $this->clientDAO->create($cliente);
+		}
+		public function read($id_cliente = ''){
+			return $this->clientDAO->read($id_cliente);
+		}
+		public function update( $cliente = array()){
+			return $this->clientDAO->update($cliente);
+		}
+		public function delete($cliente = array()){
+			return $this->clientDAO->delete($cliente);
+		}
+	}
+?>
---
+++ DatosDAO/clientesDAO.php
@@ -0,0 +1,57 @@
+<?php
+    require_once("Base.php");
+    require_once("../Modelo/clientes.php");
+
+    class clientesDAO extends Base
+    {
+        public function __construct(){
+
+        }
+
+        public function __destruct(){
+        }
+
+        public function create($cliente = array()){
+            foreach ($cliente as $key => $value) {
+                $$key = $value;
+            }
+
+            $this->query = "INSERT INTO clientes (cliente, telefono, direccion, ci, ruc, estado, fecha_alta, fecha_modificacion) 
+                            VALUES ('$cliente', '$tel', '$direccion', '$ci', '$ruc', true, CURRENT_TIMESTAMP, null)";
+            $this->set_query();
+        }
+
+        public function read($id_cliente = ''){
+            $this->query = ($id_cliente == '')? "SELECT * FROM clientes c WHERE c.estado = true"
+            :"SELECT * FROM clientes c WHERE c.id_cliente = $id_cliente";
+            $this->get_query();
+
+            $data = array();
+            foreach ($this->rows as $key => $value) {
+                array_push($data, $value);
+            }
+
+            return $data;
+        }
+
+        public function update( $cliente = array()){
+            foreach ($cliente as $key => $value) {
+                $$key = $value;
+            }
+
+            $this->query = "UPDATE clientes SET cliente ='$cliente', telefono = '$tel',
+            direccion = '$direccion', ci = '$ci', ruc = '$ruc', fecha_modificacion = CURRENT_TIMESTAMP
+            WHERE id_cliente =$id_cliente";
+            $this->set_query();
+        }
+
+        public function delete( $cliente = array()){
+            foreach ($cliente as $key => $value) {
+                $$key = $value;
+            }
+            $this->query = "UPDATE clientes SET estado = false, fecha_modificacion = CURRENT_TIMESTAMP WHERE id_cliente =$id_cliente";
+            $this->set_query();
+        }
+    }
+
+?>
