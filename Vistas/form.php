49ce9093354d2d9cc30d7daccb890349b02896d6
Lucas <lucasfrutoszarate@hotmail.com>
2022-04-24 12:06:29 -0400

    Agregado de modal en insertar articulo

--- Vistas/form.php
+++ Vistas/form.php
@@ -5,11 +5,12 @@
 	<title>Formulario</title>
 	<link rel="stylesheet" type="text/css" href="../public/assets/css/estiloform.css">
 	<link rel="stylesheet" type="text/css" href="../public/assets/css/b_superior.css">
+	<script type="text/javascript" src="../public/assets/js/main.js"></script>
 	
 </head>
 <body>
 			<!-- Trigger/Open The Modal -->
-			<button id="myBtn">Nuevo Cliente</button>
+			<button id="addNew">Nuevo Cliente</button>
 
 		<!-- The Modal -->
 		<div id="myModal" class="modal">
@@ -45,33 +46,7 @@
 
 		</div>
 
-	<script>
-		// Get the modal
-		var modal = document.getElementById("myModal");
-
-		// Get the button that opens the modal
-		var btn = document.getElementById("myBtn");
-
-		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
-
-		// When the user clicks the button, open the modal 
-		btn.onclick = function() {
-  		modal.style.display = "block";
-		}
-
-		// When the user clicks on <span> (x), close the modal
-		span.onclick = function() {
-  		modal.style.display = "none";
-		}
-
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-  			if (event.target == modal) {
-    		modal.style.display = "none";
-  			}
-		}
-	</script>
+		
 <?php
 	include("includes/footer.html");
 ?>
