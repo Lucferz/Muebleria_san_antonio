727329cea6933ec6ac7b3c47203d19780ee966f3
Lucferz <lucasfrutoszarate@hotmail.com>
2022-04-24 20:02:27 -0400

    Haciendo la tabla de Stock

--- Vistas/stock.php
+++ Vistas/stock.php
@@ -1,4 +1,8 @@
-<?php include("includes/header.html"); ?>
+<?php include("includes/header.html");
+include("../Controlador/ArticulosControl.php");
+include("../Controlador/categoriasControl.php") ;
+$articulos_control = new ArticulosControl();
+?>
    <div id="cuerpo">
       <div id="cabecera-botones">
          <div class="flexsearch">
@@ -26,22 +30,24 @@
          
          <form action="" method="POST" class="modal-form">
             <span class="close"><ion-icon name="close-outline"></ion-icon></span>
-            <h1 class="titulo-modal">Nuevo Cliente</h1>
+            <h1 class="titulo-modal">Nuevo Articulo</h1>
             
-            <p>Cliente:</p>
-            <input type="text" class="field"> <br/>
+            <p>Descripcion:</p>
+            <input type="text" name="descripcion" class="field"> <br/>
 
             <p>CI:</p>
-            <input type="text" class="field"> <br/>
+            <input type="text" name="" class="field"> <br/>
 
             <p>RUC:</p>
-            <input type="text" class="field"> <br/>
+            <input type="text" name="" class="field"> <br/>
 
             <p>Telefono:</p>
-            <input type="text" class="field"> <br/>
+            <input type="text" name="" class="field"> <br/>
 
             <p>Direccion:</p>
-            <input type="text" class="field"> <br/>
+            <input type="text" name="" class="field"> <br/>
+
+            <input type="text" name="id_articulo" hidden>
 
             <p class="center-content">
             <input type="submit" class="btn-pretty" value="GUARDAR"/>
@@ -49,6 +55,45 @@
 
          </form>
       </div>
+   </div>
+   <div id="table">
+      <table class="content-table">
+         <thead>
+            <tr>
+               <th>ID</th>
+               <th>DESCRIPCION</th>
+               <th>PRECIO DE COMPRA</th>
+               <th>PRECIO DE VENTA</th>
+               <th>EXISTENCIAS</th>
+               <th>CATEGORIA</th>
+               <th>ESTADO</th>
+               <th colspan="2">ACCIONES</th>
+            </tr>
+         </thead>
+         <tbody>
+            <?php
+               $data_articulos = $articulos_control->read();
+               foreach ($data_articulos as $key => $value) {
+                  echo "<tr>";
+                  foreach ($value as $key2 => $value2) {
+                     $$key2 = $value2;
+                  }
+                  $status = $estado?'Activo':'Inactivo';
+                  echo "<td>$id_articulo</td>
+                  <td>$descripcion</td>
+                  <td>$precio_compra</td>
+                  <td>$precio_venta</td>
+                  <td>$existencias</td>
+                  <td>$categoria</td>
+                  <td>$status</td>
+
+                  <td><a href='"./*$articulos_control->update()*/"'>Modificar</a></td>
+                  <td><a href='"./*$articulos_control->delete()*/"'></a>Desactivar</td>";
+                  echo "</tr>";
+               }
+            ?>
+         </tbody>
+      </table>
    </div>
    
 <?php include("includes/footer.html"); ?>
