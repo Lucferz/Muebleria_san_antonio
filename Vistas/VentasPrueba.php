1bded0411e6820aa487623d14455342cf83ebb2b
YisellRR <ravioloyisell@gmail.com>
2022-05-14 11:41:56 -0400

    Cambios varios en vista de vendedor

--- Vistas/VentasPrueba.php
+++ Vistas/VentasPrueba.php
@@ -7,20 +7,36 @@
    <title>Ventas</title>
 </head>
 <body>
- <img src="../public/assets/img/santo.jpg" class="logo" >
-<h2>Muebleria San Antonio</h2>
+
+    </div>
+          <div class="usuarios-select">
+          <select id="usuarios" name="usuarios">
+            <option value="no_select">Usuario Fulano</option>
+             <option value="cerrar">Cerrar sesion</option>
+            <!-- Ver para traer el usuario logueado  -->
+          </select>
+        </div>
+        <br> 
+   <header>  
+
+      <div class="logo">
+        <img src="../public/assets/img/santo.jpg">
+        <h2>Muebleria San Antonio</h2> 
+  </header>
+
 
 <div class="contenido">
-  <form action="/action_page.php">
+  <form action="/action_page.php">    
     <div class="row">
       <div class="col-25">
         <label for="clname">Cliente</label>
       </div>
       <div class="col-75"> 
               <input class="flexsearch--input" type="search" id="clname" placeholder="Cliente" >
-              <ion-icon name="add-circle-outline"></ion-icon> 
       <!--  <input type="text" id="fname" name="firstname" placeholder="Your name.."> -->
+
       </div>
+          <button class="nuevo-cliente"><ion-icon name="add-outline"></ion-icon></button>
     </div>
     <div class="row">
       <div class="col-25">
@@ -73,7 +89,7 @@
 
    <br>
     <div class="row">
-      <input type="submit" value="Confirmar">
+      <button class="confirmar">Confirmar</button>
     </div>
   </form>
 </div>
