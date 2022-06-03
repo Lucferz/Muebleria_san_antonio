dedee8e99584ef86ceb1f5259a9849040cab6da6
YisellRR <ravioloyisell@gmail.com>
2022-06-03 06:53:46 -0400

    Vista cobranzas prueba

--- Vistas/NuevaVenta.php
+++ Vistas/NuevaVenta.php
@@ -37,7 +37,7 @@
             <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
         </svg>
             </div>
-                <button class="nuevo-cliente"><ion-icon name="add-outline"></ion-icon></button>
+                 <a href="#"><button class="nuevo-cliente" ><ion-icon name="add-outline"></ion-icon> </button></a>
           </div>
           <div class="row">
             <div class="col-25">
