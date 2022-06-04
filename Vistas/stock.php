c0976c779f9147c1ac768cd88cc31d35fe6a9905
Lucas <lucasfrutoszarate@hotmail.com>
2022-06-04 04:05:54 -0400

    Cambio de tipo de input en la barra de busqueda

--- Vistas/stock.php
+++ Vistas/stock.php
@@ -10,7 +10,7 @@ $categoriasControl = new CategoriasControl();
                <div class="flexsearch--wrapper">
                   <form class="flexsearch--form" action="" method="post">
                      <div class="flexsearch--input-wrapper">
-                        <input class="flexsearch--input" type="search" id="search-box" placeholder="search">
+                        <input class="flexsearch--input" type="text" id="search-box" placeholder="Buscar">
                      </div>
                      <input class="flexsearch--submit" type="submit" value="&#10140;"/>
                   </form>
