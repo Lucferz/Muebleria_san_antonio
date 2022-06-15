b6ee29c275a0b976a449422669d7835a3c9ca214
YisellRR <ravioloyisell@gmail.com>
2022-06-15 14:57:51 -0400

    prueba menu desplegable informes

--- Vistas/includes/header.php
+++ Vistas/includes/header.php
@@ -37,6 +37,17 @@
    <a href="stock.php"><ion-icon name="storefront-outline"></ion-icon><span> Stock</span></a>
    <a href="cobranzas.php"><ion-icon name="cash-outline"></ion-icon><span> Cobro</span></a> 
    <a href="#"><ion-icon name="document-text-outline"></ion-icon><span> Informes</span></a>
+   
+   <nav>
+         <div class="dropdown">
+            <span><button class="dropbtn btn"><ion-icon name="document-text-outline"></ion-icon></button>Informes</span>
+            <div class="dropdown-content">
+               <a href="#">Informe de inventario</a><hr>
+               <a href="#">Informe de ventas</a>
+            </div>
+         </div>
+      </nav>
+
    <a href="configuracion.php"><ion-icon name="settings" size="large"></ion-icon></a>  
 
 </div>
