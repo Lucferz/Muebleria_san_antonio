67ea53bee94099ab0c906bf08af2e5caaf4946f4
YisellRR <ravioloyisell@gmail.com>
2022-06-22 20:47:17 -0400

    Agg lib fpdf

---
+++ Vistas/inf2.php
@@ -0,0 +1,18 @@
+<?php
+
+require('../inf/fpdf/fpdf.php');
+
+$pdf=new FPDF();
+$pdf->AddPage();
+
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(50,10,'Probando FPDF',1,1,'L');
+
+$pdf->Cell(50,5,'salto de linea',1,1);
+
+$pdf->SetFont('Arial','B',10);
+$pdf->MultiCell(190,5,'Lorem Ipsum o cualquier texto de relleno. .');
+
+$pdf->Output();
+
+?>
--- Vistas/informes.php
+++ Vistas/informes.php
@@ -1,13 +1,67 @@
-<?php include("includes/header.html");
-include("includes/footer.html");
-
+<?php 
+require '../vendor/autoload.php';
+include("../Controlador/ArticulosControl.php");
+include("../Controlador/categoriasControl.php") ;
+include("../Controlador/taxesControl.php") ;
+$articulos_control = new ArticulosControl();
+$categoriasControl = new CategoriasControl();
+$taxControl = new taxesControl();
+ob_start(); //captura la salida
 ?>
-         <link rel="stylesheet" type="text/css" href="../public/assets/css/styles.css">
-         <div id="informes-buttons">
-         	<button id="addNew" class="btn-pretty"><ion-icon name="document-text-outline"></ion-icon> Informe de stock</button>
-            <br>
-            <button id="addNew" class="btn-pretty"><ion-icon name="document-text-outline"></ion-icon> Informe de ventas</button>
-         </div>
+<!DOCTYPE html>
+ <html>
+ <head>
+    <meta charset="utf-8">
+    <title>prueba</title>
+ </head>
+ <body>
+  <div id="table">
+      <table class="content-table">
+         <thead>
+            <tr>
+               <th>DESCRIPCION</th>
+               <th>EXISTENCIAS</th>
+               <th colspan="2">ACCIONES</th>
+            </tr>
+         </thead>
+         <tbody id="tablaDatos">
+            <?php
+               $data_articulos = $articulos_control->read();
+               foreach ($data_articulos as $key => $value) {
+                  echo "<tr>";
+                  foreach ($value as $key2 => $value2) {
+                     $$key2 = $value2;
+                  }
+                  if($estado){
+                     $status = 'Activo';
+                     $btnDelLabel = 'Desactivar';
+                  }else{
+                     $status = 'Inactivo';
+                     $btnDelLabel = 'Reactivar';
+                  }
+                  echo "<div id='row-content'>
+                  <td>$descripcion</td>
+                  <td>$existencias</td>
+                  </div>
+                  ";
+                  echo "</tr>";
+               }
+            ?>
+         </tbody>
+      </table>
+   </div>
+   <script type="text/javascript" src="../public/assets/js/stock.js"></script>
+ </body>
+ </html> 
+
+ 
+<?php 
+
+$dompdf = new Dompdf\Dompdf();
+$dompdf->loadHtml(ob_get_clean());
+
+$dompdf->render();
+$dompdf->stream("informe_stock.pdf", array("Attachment"=>false));
 
 
-
+ ?>
