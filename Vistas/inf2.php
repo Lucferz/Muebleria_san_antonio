78e3f1f4a724cd787faea79223325d9a3a9c780a
YisellRR <ravioloyisell@gmail.com>
2022-06-23 15:59:34 -0400

    correcion posicion informe stock

--- Vistas/inf2.php
+++ Vistas/inf2.php
@@ -14,7 +14,7 @@ function Header()
     $this->Ln(20); 
    
     $this->SetFont('Arial','B',12);  
-    $this->Cell(30); 
+    $this->Cell(25); 
     $this->Cell(100, 10, 'Descripcion', 1, 0, 'C', 0);
     $this->Cell(40, 10, 'Existencias', 1, 1 , 'C', 0);
 }
@@ -40,7 +40,7 @@ $pdf->AliasNbPages();
 $pdf->AddPage();
 
 while ($row = $resultado->fetch_assoc()) {
-    $pdf->Cell(30); 
+    $pdf->Cell(25); 
 	$pdf->Cell(100, 10, $row['descripcion'], 1, 0, 'C', 0);
 	$pdf->Cell(40, 10, $row['existencias'], 1, 1 , 'C', 0);
 }
