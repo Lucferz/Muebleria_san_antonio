b3ddf909577453373df0ccdfcd7e638bd0871764
Yudith12 <yudith199616gonza@gmail.com>
2022-06-27 19:11:40 -0400

    mod barra lateral

--- Vistas/includes/header.php
+++ Vistas/includes/header.php
@@ -24,19 +24,19 @@ if ($_SESSION['autenticado'] != true) {
       <center>
          <img src="../public/assets/img/santo.jpg" class="foto" alt=" ">
       </center>
-      <a href="inicio.php?i=1" id="inicio" <?php echo $_REQUEST['i']=="1"?"class = 'navbaractive'":"" ?> >
+      <a href="inicio.php" id="inicio" <?php echo $_SERVER['REQUEST_URI']=="/Muebleria_san_antonio/Vistas/inicio.php"?"class = 'navbaractive'":"" ?> >
          <ion-icon name="home-outline"></ion-icon><span> Inicio</span>
       </a>
-      <a href="Cliente.php?i=2" <?php echo $_REQUEST['i']=="2"?"class = 'navbaractive'":"" ?>>
+      <a href="Cliente.php" <?php echo $_SERVER['REQUEST_URI']=="/Muebleria_san_antonio/Vistas/Cliente.php"?"class = 'navbaractive'":"" ?>>
          <ion-icon name="person-outline"></ion-icon><span> Clientes</span>
       </a>
-      <a href="ventas.php?i=3" <?php echo $_REQUEST['i']=="3"?"class = 'navbaractive'":"" ?>>
+      <a href="ventas.php" <?php echo $_SERVER['REQUEST_URI']=="/Muebleria_san_antonio/Vistas/ventas.php"?"class = 'navbaractive'":"" ?>>
          <ion-icon name="bag-outline"></ion-icon><span> Ventas</span>
       </a>
-      <a href="stock.php?i=4" <?php echo $_REQUEST['i']=="4"?"class = 'navbaractive'":"" ?>>
+      <a href="stock.php" <?php echo $_SERVER['REQUEST_URI']=="/Muebleria_san_antonio/Vistas/stock.php"?"class = 'navbaractive'":"" ?>>
          <ion-icon name="storefront-outline"></ion-icon><span> Stock</span>
       </a>
-      <a href="cobranzasview.php?i=5" <?php echo $_REQUEST['i']=="5"?"class = 'navbaractive'":"" ?>>
+      <a href="cobranzasview.php" <?php echo $_SERVER['REQUEST_URI']=="/Muebleria_san_antonio/Vistas/cobranzasview.php"?"class = 'navbaractive'":"" ?>>
          <ion-icon name="cash-outline"></ion-icon><span> Cobro</span>
       </a>
       <nav>
@@ -56,7 +56,7 @@ if ($_SESSION['autenticado'] != true) {
          // var_dump($url);
          // echo '<pre>'; print_r($url); echo '</pre>';
       ?>
-      <a href="configuracion.php?i=6" <?php echo $_REQUEST['i']=="6"?"class = 'navbaractive'":"" ?>>
+      <a href="configuracion.php"  <?php echo $_SERVER['REQUEST_URI']=="/Muebleria_san_antonio/Vistas/configuracion.php"?"class = 'navbaractive'":"" ?>>
          <ion-icon name="settings" size="large"></ion-icon>
       </a>
 
